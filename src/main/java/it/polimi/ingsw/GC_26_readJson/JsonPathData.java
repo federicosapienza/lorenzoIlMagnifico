84f549676f6e8fd24913763eafc4e014d4e8fbf9
Leonardo Varè <Leonardo Var>
2017-06-05 22:33:04 +0200

    TerritoryCards read completed

---
+++ src/main/java/it/polimi/ingsw/GC_26_readJson/JsonPathData.java
@@ -0,0 +1,27 @@
+package it.polimi.ingsw.GC_26_readJson;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class JsonPathData {
+	String[] territoryCardsPeriod1PathArray = {"src/Cards/Territory_cards/Period_1/Citadel.json","src/Cards/Territory_cards/Period_1/City.json","src/Cards/Territory_cards/Period_1/Commercial_Hub.json","src/Cards/Territory_cards/Period_1/Forest.json","src/Cards/Territory_cards/Period_1/Gravel_Pit.json","src/Cards/Territory_cards/Period_1/Monastery.json","src/Cards/Territory_cards/Period_1/Village.json","src/Cards/Territory_cards/Period_1/Woods.json"};
+	String[] territoryCardsPeriod2PathArray = {"src/Cards/Territory_cards/Period_2/Dukedom.json","src/Cards/Territory_cards/Period_2/Estate.json","src/Cards/Territory_cards/Period_2/Gold_Mine.json","src/Cards/Territory_cards/Period_2/Hermitage.json","src/Cards/Territory_cards/Period_2/Manor_House.json","src/Cards/Territory_cards/Period_2/Mining_Town.json","src/Cards/Territory_cards/Period_1/Mountain_Town.json","src/Cards/Territory_cards/Period_2/Rock_Pit.json"};
+	String[] territoryCardsPeriod3PathArray = {"src/Cards/Territory_cards/Period_3/Castle.json","src/Cards/Territory_cards/Period_3/Colony.json","src/Cards/Territory_cards/Period_3/Farm.json","src/Cards/Territory_cards/Period_3/Fortified_Town.json","src/Cards/Territory_cards/Period_3/Marble_Pit.json","src/Cards/Territory_cards/Period_3/Province.json","src/Cards/Territory_cards/Period_3/Sanctuary.json","src/Cards/Territory_cards/Period_3/Trading_Town.json"};
+
+	
+	
+	public String[] getTerritoryCardsPeriod1PathArray() {
+		return territoryCardsPeriod1PathArray;
+	}
+	public String[] getTerritoryCardsPeriod2PathArray() {
+		return territoryCardsPeriod2PathArray;
+	}
+	public String[] getTerritoryCardsPeriod3PathArray() {
+		return territoryCardsPeriod3PathArray;
+	}
+
+}
+	
+	
+
--- src/main/java/it/polimi/ingsw/GC_26_readJson/ReadTerritoryCard.java
+++ src/main/java/it/polimi/ingsw/GC_26_readJson/ReadTerritoryCard.java
@@ -4,60 +4,71 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.lang.reflect.Type;
-import java.util.Arrays;
-import java.util.List;
+import it.polimi.ingsw.GC_26_utilities.resourcesAndPoints.*;
+
 
 import com.google.gson.*;
-import com.google.gson.reflect.TypeToken;
 
 import it.polimi.ingsw.GC_26_cards.developmentCards.DevelopmentCard;
 import it.polimi.ingsw.GC_26_cards.developmentCards.DevelpmentCardImplementation;
 import it.polimi.ingsw.GC_26_cards.effects.ReceiveResourcesOrPointsEffect;
-import it.polimi.ingsw.GC_26_utilities.resourcesAndPoints.ResourcesOrPoints;
+import it.polimi.ingsw.GC_26_cards.leaderCard.PointsOrResourcesRequirement;
+import it.polimi.ingsw.GC_26_utilities.resourcesAndPoints.Points;
 
-public class ReadTerritoryCards {
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+
+import com.google.gson.reflect.TypeToken;
+
+
+public class ReadTerritoryCard {
 		
 	private String name;
 	private int period;
-	private List<Integer> immediateResourcesAndPointsList; 
-	private List<Integer> permanentResourcesAndPointsList;
+	private List<Integer> immediateResourcesAndPointsList = new ArrayList<Integer>(); 
+	private List<Integer> permanentResourcesAndPointsList = new ArrayList<Integer>();
 	private int actionValue;
 	private BufferedReader br= null;
 	private Gson gson = new Gson();
 	private JsonObject jsonObject= null;
 	private JsonElement jsonElement= null;
-	private List<DevelopmentCard> territoryCards;//getter!
-	
-	
+	private List<DevelopmentCard> territoryCards = new ArrayList<DevelopmentCard>();//getter! -> non credo sia utile,passo i file direttamente ad un'altra classe
+	private JsonPathData jsonPathData = new JsonPathData();
+
 		public static void main(String [] args){
-			ReadTerritoryCards rtc = new ReadTerritoryCards();
-			rtc.readCards();
+			ReadTerritoryCard rtc = new ReadTerritoryCard();
+			rtc.readCards(1);//per ora li metto manualmente qua i valori
 		}
 		
-		private void readCards(){
-				createJsonObjectFromFile();
-				readName();
-				readPeriod();
-				readImmediateResourcesAndPoints();
-				readPermanentResourcesAndPoints();
-				readActionValue();
-				stamp();
-				createTerritoryCard();
-				if(br!= null){
-					try {
-						br.close();
+		private void readCards(int numberOfList){
+				String[] ListOfPaths = chooseListOfCards(numberOfList);
+				for(int i=0;i<8;i++){
+					createJsonObjectFromFile(ListOfPaths[i]);
+					readName();
+					readPeriod();
+					readActionValue();
+					readImmediateResourcesAndPoints();
+					readPermanentResourcesAndPoints();
+					stamp(); 
+					createTerritoryCard();
+					if(br!= null){
+						try {
+							br.close();
+							}
+						catch (IOException e) {
+							e.printStackTrace();
 						}
-					catch (IOException e) {
-						e.printStackTrace();
 					}
 				}
 			}
 		
 		
-		private void createJsonObjectFromFile(){
+		private void createJsonObjectFromFile(String path){
 			try {
-				br = new BufferedReader(new FileReader("TerritoryCards.json"));
+				br = new BufferedReader(new FileReader(path));
 				jsonObject= gson.fromJson(br, JsonObject.class);
 			} catch (FileNotFoundException e) {e.printStackTrace();}
 		}
@@ -78,7 +89,6 @@ public class ReadTerritoryCards {
 									Type listType = new TypeToken<List<Integer>>() {}.getType();
 									immediateResourcesAndPointsList = new Gson().fromJson(jsonObject.get("immediateResourcesAndPoints"), listType);
 								  }
-				// ResourcesOrpoints.newPoint(0,0,0,immediateMilitartPointsInt)
 			else{ 
                  initializeToZero(1);
 				}
@@ -125,5 +135,25 @@ public class ReadTerritoryCards {
 		    DevelopmentCard developmentCard= DevelpmentCardImplementation.territoryCard(name, period, null, new ReceiveResourcesOrPointsEffect(ResourcesOrPoints.newResourcesOrPoints(immediateResourcesAndPointsList.get(0), immediateResourcesAndPointsList.get(1), immediateResourcesAndPointsList.get(2), immediateResourcesAndPointsList.get(3), immediateResourcesAndPointsList.get(4), immediateResourcesAndPointsList.get(5), immediateResourcesAndPointsList.get(6), immediateResourcesAndPointsList.get(7))), new ReceiveResourcesOrPointsEffect(ResourcesOrPoints.newResourcesOrPoints(permanentResourcesAndPointsList.get(0), permanentResourcesAndPointsList.get(1), permanentResourcesAndPointsList.get(2), permanentResourcesAndPointsList.get(3), permanentResourcesAndPointsList.get(4), permanentResourcesAndPointsList.get(5), permanentResourcesAndPointsList.get(6), permanentResourcesAndPointsList.get(7))), actionValue);
 			territoryCards.add(developmentCard);
 		}
+		
+		private String[] chooseListOfCards(int numOfList){
+			switch(numOfList) {
+			case 1:
+				return jsonPathData.getTerritoryCardsPeriod1PathArray();
+			case 2:
+				return jsonPathData.getTerritoryCardsPeriod2PathArray();
+			case 3:
+				return jsonPathData.getTerritoryCardsPeriod3PathArray();
+			//TODO
+			default:
+				throw new IllegalArgumentException();
+			}
+		}
+		
+		
+		/*
+		public List<DevelopmentCard> getTerritoryCards() {
+			return territoryCards;
+		}*///non so se servirà,anzi credo proprio di no in quanto vorrei mettere ciò che leggo in Cards
 		
 }
