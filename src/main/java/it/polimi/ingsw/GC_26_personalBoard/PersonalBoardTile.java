3faaae80fa67cc37e084cb6ff1412ece69763944
David Yun <yvid@MacBook-Pro-di-David.local>
2017-06-08 16:56:07 +0200

    File pom.xml modified. Now json should work with no problems. Continuing javadoc.

--- src/main/java/it/polimi/ingsw/GC_26_personalBoard/PersonalBoardTile.java
+++ src/main/java/it/polimi/ingsw/GC_26_personalBoard/PersonalBoardTile.java
@@ -4,32 +4,71 @@ import it.polimi.ingsw.GC_26_gameLogic.GameParameters;
 import it.polimi.ingsw.GC_26_player.Player;
 import it.polimi.ingsw.GC_26_utilities.resourcesAndPoints.ResourcesOrPoints;
 
+/**
+ * 
+ * @author David Yun (david.yun@mail.polimi.it)
+ * @author Federico Sapienza (federico.sapienza@mail.polimi.it)
+ * @author Leonardo Varè (leonardo.vare@mail.polimi.it)
+ *
+ * This class represents the personal bonus tile at the left of each personal board. 
+ */
 public class PersonalBoardTile {
+	
+	// resources or points given when production is done.
 	private ResourcesOrPoints resourcesOrPointsProduction;
+	
+	// resources or points given when harvest is done.
 	private ResourcesOrPoints resourcesOrPointsHarvest;
+	
+	// required value to activate the personal bonus tile
 	private final int value=GameParameters.getStandardPersonalBonusTileValue();
 	
-	public PersonalBoardTile(ResourcesOrPoints resourcesOrPointsProoduction,ResourcesOrPoints resourcesOrPointsHarvest){
-		this.resourcesOrPointsProduction=resourcesOrPointsProoduction;
+	/**
+	 * Constructor of the personal bonus tile: parameters denote whether the rules are basic or advanced.
+	 * @param resourcesOrPointsProduction It represents the resources or points given when production action is performed.
+	 * @param resourcesOrPointsHarvest It represents the resources or points given when harvest action is performed.
+	 */
+	public PersonalBoardTile(ResourcesOrPoints resourcesOrPointsProduction,ResourcesOrPoints resourcesOrPointsHarvest){
+		this.resourcesOrPointsProduction=resourcesOrPointsProduction;
 		this.resourcesOrPointsHarvest=resourcesOrPointsHarvest;
 	}
 	
+	/**
+	 * Method that gives the harvest bonus to the player, when he performs a harvest action
+	 * @param player It's the player that has performed a harvest action and so activates the harvest bonus
+	 */
 	public void giveHarvestBonus(Player player){
 		player.getWarehouse().add(resourcesOrPointsHarvest);
 	}
 	
+	/**
+	 * Method that gives the production bonus to the player, when he performs a production action
+	 * @param player It's the player that has performed a production action and so activates the production bonus
+	 */
 	public void giveProductionBonus(Player player){
 		player.getWarehouse().add(resourcesOrPointsProduction);
 	}
 	
-	public ResourcesOrPoints getResourcesOrPointsProduction(){
-		return resourcesOrPointsProduction;
-	}
-	
+	/**
+	 * Method that returns the resources or points given by this personal bonus tile when harvest action is performed 
+	 * @return resourcesOrPointsHarvest It represents the resources or points given by this personal bonus tile when harvest action is performed 
+	 */
 	public ResourcesOrPoints getResourcesOrPointsHarvest(){
 		return resourcesOrPointsHarvest;
 	}
 	
+	/**
+	 * Method that returns the resources or points given by this personal bonus tile when production action is performed 
+	 * @return resourcesOrPointsProduction It represents the resources or points given by this personal bonus tile when production action is performed 
+	 */
+	public ResourcesOrPoints getResourcesOrPointsProduction(){
+		return resourcesOrPointsProduction;
+	}
+	
+	/**
+	 * Method that returns the required value to activate this personal bonus tile
+	 * @return value It's the required value to activate this personal bonus tile
+	 */
 	public int getValue(){
 		return value;
 	}
