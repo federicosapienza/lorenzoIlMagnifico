e6c6e5464ef4acf9a0b29df124ffa314864c5b18
David Yun <yvid@MBP-di-David.station>
2017-06-15 17:45:48 +0200

    Continuing and updating JavaDoc

--- src/main/java/it/polimi/ingsw/GC_26_readJson/BonusInterface.java
+++ src/main/java/it/polimi/ingsw/GC_26_readJson/BonusInterface.java
@@ -6,6 +6,15 @@ import java.util.Map;
 import it.polimi.ingsw.GC_26_personalBoard.PersonalBoardTile;
 import it.polimi.ingsw.GC_26_utilities.resourcesAndPoints.ResourcesOrPoints;
 
+/**
+ * 
+ * @author David Yun (david.yun@mail.polimi.it)
+ * @author Federico Sapienza (federico.sapienza@mail.polimi.it)
+ * @author Leonardo Varè (leonardo.vare@mail.polimi.it)
+ * 
+ * Interface for the bonus logic.
+ *
+ */
 public interface BonusInterface {
 	List<ResourcesOrPoints> getResourcesOrPointsStarting();
 	List<ResourcesOrPoints[]> getListOfResourcesOfPointsArray();
--- src/main/java/it/polimi/ingsw/GC_26_readJson/TimerValuesInterface.java
+++ src/main/java/it/polimi/ingsw/GC_26_readJson/TimerValuesInterface.java
@@ -1,5 +1,12 @@
 package it.polimi.ingsw.GC_26_readJson;
 
+/**
+ * @author David Yun (david.yun@mail.polimi.it)
+ * @author Federico Sapienza (federico.sapienza@mail.polimi.it)
+ * @author Leonardo Varè (leonardo.vare@mail.polimi.it)
+ * Interface for the various timers that are needed during the game
+ *
+ */
 public interface TimerValuesInterface {
 	int getStartingTimer();
 	int getTurnTimer();
