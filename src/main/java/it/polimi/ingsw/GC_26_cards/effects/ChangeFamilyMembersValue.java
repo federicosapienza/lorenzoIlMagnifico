3514a063d4362e0e5d18a80d491723386287d1b6
David Yun <yvid@MBP-di-David.station>
2017-06-27 01:22:14 +0200

    Continuing JavaDoc

--- src/main/java/it/polimi/ingsw/GC_26_cards/effects/ChangeFamilyMembersValue.java
+++ src/main/java/it/polimi/ingsw/GC_26_cards/effects/ChangeFamilyMembersValue.java
@@ -1,8 +1,14 @@
 package it.polimi.ingsw.GC_26_cards.effects;
 
 import it.polimi.ingsw.GC_26_player.Player;
-/*Represent a change in value of a family member;
- * use negative value for decrement, positive for increment
+/**
+ * 
+ * @author David Yun (david.yun@mail.polimi.it)
+ * @author Federico Sapienza (federico.sapienza@mail.polimi.it)
+ * @author Leonardo Varè (leonardo.vare@mail.polimi.it)
+ * 
+ * This class represents the effect that changes the value of a family member.
+ * negative values are used to decrease the value, positive ones to increase it
  * 
  */
 
@@ -13,11 +19,19 @@ public class ChangeFamilyMembersValue implements Effect{
 	private final int colouredMemberChange;
 	private final int neutralMemberChange;
 
+	/**
+	 * Constructor: it creates the effect with the changes to apply onto the coloured and neutral family members, as expressed in the parameters
+	 * @param colouredMemberChange It's the value to add to the actual value of the coloured family members
+	 * @param neutralMemberChange It's the value to add to the actual value of the neutral family member
+	 */
 	public ChangeFamilyMembersValue(int colouredMemberChange, int neutralMemberChange) {
 		this.colouredMemberChange = colouredMemberChange;
 		this.neutralMemberChange= neutralMemberChange;
 	}
 	
+	/**
+	 * Method called to apply the effect to the player who is involved in the effect
+	 */
 	@Override
 	public synchronized void doEffect(Player player, boolean immediate) {
 		player.getPermanentModifiers().setColouredMembersChange(colouredMemberChange);
@@ -26,6 +40,9 @@ public class ChangeFamilyMembersValue implements Effect{
 		
 	}
 	
+	/**
+	 * Method that explains the effect as a string message
+	 */
 	@Override
 	public String toString() {
 		if(neutralMemberChange==0)
--- src/main/java/it/polimi/ingsw/GC_26_cards/effects/DeletingBonusFloorsEffect.java
+++ src/main/java/it/polimi/ingsw/GC_26_cards/effects/DeletingBonusFloorsEffect.java
@@ -2,15 +2,29 @@ package it.polimi.ingsw.GC_26_cards.effects;
 
 import it.polimi.ingsw.GC_26_player.Player;
 
-//Preacher effects: revoke the chance of getting bonuses from towers positions
+/**
+ * 
+ * @author David Yun (david.yun@mail.polimi.it)
+ * @author Federico Sapienza (federico.sapienza@mail.polimi.it)
+ * @author Leonardo Varè (leonardo.vare@mail.polimi.it)
+ * 
+ * This class represents the one of effects applied by Preacher: it revokes the chance to get bonuses from towers positions
+ *
+ */
 
 public class DeletingBonusFloorsEffect implements Effect{
 
+	/**
+	 * Method called to apply the effect to the player who is involved in the effect
+	 */
 	@Override
 	public synchronized void doEffect(Player player, boolean immediate) {
 		player.getPermanentModifiers().setBonusRevokedOn();  
 	}
 	
+	/**
+	 * Method that explains the effect as a string message
+	 */
 	@Override
 	public String toString() {
 		return "revoke the chance of getting bonuses from towers positions";
